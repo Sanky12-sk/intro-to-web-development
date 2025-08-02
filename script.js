1677e9a00957d990a84f9042e03340e65ce6bd32
Sanky12-sk <douniasanky@gmail.com>
2025-08-02 11:54:09 -0700

    secondary committed

---
+++ script.js
@@ -0,0 +1,18 @@
+function changeBackgroundColor(color) {
+        let section = document.getElementById("coding-journey");
+        section.style.backgroundColor = color;
+      }
+
+      console.log("Script loaded successfully");
+
+      document
+        .getElementById("intro")
+        .addEventListener("click", () => changeBackgroundColor("#2CA02C"));
+
+      document
+        .getElementById("react")
+        .addEventListener("click", () => changeBackgroundColor("#FF7F0E"));
+
+      document
+        .getElementById("backend")
+        .addEventListener("click", () => changeBackgroundColor("#9467BD"));
