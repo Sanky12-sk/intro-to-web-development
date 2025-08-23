2169cf77ea59b4d132f720c0f162f500834d0767
Sanky12-sk <douniasanky@gmail.com>
2025-08-23 11:17:37 -0700

    4

---
+++ greater number with function.js
@@ -0,0 +1,13 @@
+
+//greater number with function
+function greaterNum(){
+var s=34;
+var d=3234;
+if(s>d){
+   return "The greater number of "+s+" and "+d+" is "+s;
+}
+else{
+    return "The greater number of "+s+" and "+d+" is "+d;
+}
+}
+console.log(greaterNum());
