ff26d6fd513685c79fa3b9a3590d391684096ad5
Sanky12-sk <douniasanky@gmail.com>
2025-08-23 11:40:42 -0700

    1234

---
+++ properties of a circle.js
@@ -0,0 +1,8 @@
+//calculate circumference and area of a circle 
+var radius=4;
+var circonference= 3.14*2*radius;
+var area= Math.PI * radius*radius;
+
+console.log("circumference: " +circonference );
+
+console.log("The area is " + area);
