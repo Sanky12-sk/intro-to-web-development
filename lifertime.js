d24b0ecc109ddb271f890f21dafa5bcb2fc8dbf7
Sanky12-sk <douniasanky@gmail.com>
2025-08-23 11:58:16 -0700

    $

---
+++ lifertime.js
@@ -0,0 +1,5 @@
+var currentAge=20;
+var maxAge=110;
+var foodPerDay=3;
+var totalNeeded = (foodPerDay * 365) * (maxAge - currentAge);
+console.log('You will need ' + totalNeeded + ' foods to last you until the ripe old age of ' + maxAge);
