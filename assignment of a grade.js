864807a7d55847c05fd948f36dd08351aa8c5014
Sanky12-sk <douniasanky@gmail.com>
2025-08-23 10:50:10 -0700

    123

---
+++ assignment of a grade.js
@@ -0,0 +1,16 @@
+//Write a program that assigns a letter grade based on a numerical grade.
+var score = 75;
+var grade;
+
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+} else if (score >= 70) {
+  grade = "C";
+} else if (score >= 60) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+console.log("Grade: " + grade);
