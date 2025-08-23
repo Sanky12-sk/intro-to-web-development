d7880aa1ba1c2638f97185794e0789cf364270ad
Sanky12-sk <douniasanky@gmail.com>
2025-08-23 10:09:19 -0700

    testing

---
+++ sign of product.js
@@ -0,0 +1,8 @@
+//Write a JavaScript conditional statement to find the sign of the product of three numbers. Display an alert box with the specified sign.
+var product=3*-7*2;
+if (product>0){
+    console.log("ANswer:"+ product + ",the sign is +");
+}
+else{
+    console.log("ANswer:"+ product + ",the sign is -");
+}
