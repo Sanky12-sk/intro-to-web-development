243b7cf3bbbe50701c047e17db5c80250edda7a0
Sanky12-sk <douniasanky@gmail.com>
2025-08-24 12:17:05 -0700

    de

---
+++ geometrize cycle.js
@@ -0,0 +1,10 @@
+function calcCircumfrence(radius){
+    circonference= Math.PI*2*radius;
+    console.log('The circumference is :'+circonference);
+}
+function area(radius){
+    area= Math.PI * radius*radius;
+    console.log('The area is:'+area );
+}
+calcCircumfrence(6);
+area(4);
